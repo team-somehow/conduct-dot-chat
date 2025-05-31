71a85720a001d2e60d41a8a7271989c24f98fd48
VinayKanse <vinaykanse9451@gmail.com>
2025-05-31 16:01:43 +0200

    refactor: replace AuthProvider with DynamicContextProvider and WagmiProvider in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,25 @@ import {
   NotificationProvider,
   TransactionPopupProvider,
 } from "@blockscout/app-sdk";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
+import { hederaTestnet, polygonAmoy } from "viem/chains";
+import { createConfig, http, WagmiProvider } from "wagmi";
 import { router } from "./app/router";
-import { AuthProvider } from "./context/global/auth/AuthContext";
 import { ThemeProvider } from "./context/global/theme/ThemeContext";
+
+const config = createConfig({
+  chains: [polygonAmoy, hederaTestnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [polygonAmoy.id]: http(),
+    [hederaTestnet.id]: http(),
+  },
+});
 
 // TODO(App):
 // 1. Add global error boundary
@@ -22,16 +35,25 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <NotificationProvider>
-          <TransactionPopupProvider>
-            <ThemeProvider>
-              <RouterProvider router={router} />
-              <Toaster position="top-right" richColors closeButton />
-            </ThemeProvider>
-          </TransactionPopupProvider>
-        </NotificationProvider>
-      </AuthProvider>
+      <DynamicContextProvider
+        settings={{
+          environmentId: "98669622-de8f-4b3b-8aaf-e61ec95f5e23",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <WagmiProvider config={config}>
+          <DynamicWagmiConnector>
+            <NotificationProvider>
+              <TransactionPopupProvider>
+                <ThemeProvider>
+                  <RouterProvider router={router} />
+                  <Toaster position="top-right" richColors closeButton />
+                </ThemeProvider>
+              </TransactionPopupProvider>
+            </NotificationProvider>
+          </DynamicWagmiConnector>
+        </WagmiProvider>
+      </DynamicContextProvider>
     </QueryClientProvider>
   );
 };
