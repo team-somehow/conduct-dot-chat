1a8045b76cea6c8ff9869da81119cb52286b11c4
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2025-05-31 07:34:48 +0200

    chore: comment out autorunning code

--- agents/nft-deployer/scripts/deploy.ts
+++ agents/nft-deployer/scripts/deploy.ts
@@ -1,16 +1,16 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
+// async function main() {
+//   const [deployer] = await ethers.getSigners();
+//   console.log("Deploying contracts with the account:", deployer.address);
 
-  const NFT = await ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy("Test NFT", "TEST-NFT", deployer.address);
+//   const NFT = await ethers.getContractFactory("NFT");
+//   const nft = await NFT.deploy("Test NFT", "TEST-NFT", deployer.address);
 
-  await nft.waitForDeployment();
+//   await nft.waitForDeployment();
 
-  console.log("NFT contract deployed to:", await nft.getAddress());
-}
+//   console.log("NFT contract deployed to:", await nft.getAddress());
+// }
 
 export async function deployNFT(name: string, symbol: string) {
   const [deployer] = await ethers.getSigners();
@@ -23,9 +23,9 @@ export async function deployNFT(name: string, symbol: string) {
   return nft;
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
